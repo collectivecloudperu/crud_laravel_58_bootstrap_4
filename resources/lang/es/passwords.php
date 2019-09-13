efd5c069d2762c337905fe9ec274028fadee5c07
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-09-13 15:31:20 -0500

    Add files via upload

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Las contraseñas deben contener al menos 6 caracteres y coincidir.',
+
+    'user' => 'No podemos encontrar a un usuario con ese correo electrónico.',
+
+    'token' => 'Eltoken de recuperación de contraseña es inválido.',
+
+    'sent' => '¡El Enlace para resetear tu Contraseña ha sido enviado!',
+
+    'reset' => '¡Tu contraseña ha sido restablecida! Puedes iniciar Sesión:',
+
+];
